735fec00241037a1a8943187bb9a29bec8389636
Vesa Kelani <vesakelani@hotmail.co.uk>
2019-09-03 21:19:51 +0100

    Save most recent tweet id and make it run on its own

--- bot.py
+++ bot.py
@@ -1,85 +1,51 @@
 import tweepy # version 3.8.0
 from credentials import *
 import random
-# from requests.exceptions import Timeout, ConnectionError
-# from requests.packages.urllib3.exceptions import ReadTimeoutError, ProtocolError
-# import time
+from requests.exceptions import Timeout, ConnectionError
+from requests.packages.urllib3.exceptions import ReadTimeoutError, ProtocolError
+import time
+
+BOT_USER_NAME = "NCTsongbot"
 
 def has_tweet_been_replied_to(user_name, tweet_id):
-    for reply in tweepy.Cursor(api.search, q = "to:" + user_name, since_id = tweet_id).items():
+    for reply in tweepy.Cursor(api.search, q = "@" + user_name, since_id = tweet_id).items():
         print("Found reply by:@" + reply.user.screen_name + " to:@" + user_name)
-
         if reply.in_reply_to_status_id_str == tweet_id:
-            if reply.user.screen_name == "NCTsongbot":
+            if reply.user.screen_name == BOT_USER_NAME:
                 return True
-
     return False
 
-# logs into the acc
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
-# checks authentication
 try:
     api.verify_credentials()
     print("Authentication OK")
 except BaseException as e:
     print(e.reason)
 
-# splits the lines of the txt file with all the links of the songs
-lines = open('songs.txt').read().splitlines()
 
-latest_tweet_id = "1167466263960018944"
+songs = open('songs.txt').read().splitlines()
 
-# searches for any tweet that includes '@NCTsongbot'
-for tweet in tweepy.Cursor(api.search, q='@NCTsongbot', since_id=latest_tweet_id).items(1):
-    print("Found tweet by:@" + tweet.user.screen_name)
-    if has_tweet_been_replied_to(tweet.user.screen_name, tweet.id_str) == False:
-        # api.update_status("@" + tweet.user.screen_name + " " + random.choice(lines), tweet.id)
-        print("responded to @" + tweet.user.screen_name)
+while True:
+    latest_tweet_id = open('latest_tweet_id.txt').read()
 
+    try:
+        for tweet in tweepy.Cursor(api.search, q="@" + BOT_USER_NAME, since_id=latest_tweet_id).items():
+            print("Found tweet by @" + tweet.user.screen_name)
+            api.update_status("@" + tweet.user.screen_name + " " + random.choice(songs), tweet.id)
+            print("Responded to @" + tweet.user.screen_name)
 
-#END
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #         print("Already replied to:@" + tweet.user.screen_name)
-    #     else: # tweets at the user a link/line chosen randomly from the txt file
-    #         api.update_status("@" + tweet.user.screen_name + " " + random.choice(lines))
-    #         print("responded to @" + tweet.user.screen_name)
-
-
-# errors
-    # except (tweepy.TweepError, tweepy.RateLimitError) as e:
-    #     print(e.reason)
-    #     time.sleep(5)
-    #     continue
-    # except (Timeout, ssl.SSLError, ConnectionError, ReadTimeoutError, ProtocolError) as exc:
-    #     continue
-
-# in terminal
-# to keep bot going when laptop off
-# nohup python3 bot.py &
-# when recieve a string of numbers
-# logout
-# stop it with kill *numbers*
-
-# class MyStreamListener(tweepy.StreamListener):
-#
-#     def on_status(self, status):
-#         print(status.text)
-
-# myStreamListener = MyStreamListener()
-# myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+            with open('latest_tweet_id.txt', 'w') as latest_tweet_id_file:
+                latest_tweet_id_file.write(tweet.id_str)
+    except (
+        tweepy.TweepError,
+        tweepy.RateLimitError,
+        Timeout,
+        ConnectionError,
+        ReadTimeoutError,
+        ProtocolError
+            ) as e:
+        print(e.reason)
+        time.sleep(60)
